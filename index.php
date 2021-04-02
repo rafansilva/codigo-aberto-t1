435c445a65c12960817952d163f33a3bc58c5c90
Rafael N. Silva <rafaelnascimento0505@gmail.com>
2021-04-01 22:57:46 -0300

    Add .gitignore

--- index.php
+++ index.php
@@ -4,6 +4,18 @@ session_start();
 
 require __DIR__ . "/vendor/autoload.php";
 
-echo "<h1>Olá Mundo! Esse é o código aberto!</h1>";
+use CoffeeCode\Router\Router;
+
+$router = new Router(site());
+
+/**
+ * WEB ROUTES
+ */
+$router->group(null);
+$router->get("/", "Web:login", "web.login");
+$router->get("/cadastrar", "Web:register", "web.register");
+$router->get("/recuperar", "Web:forget", "web.forget");
+$router->get("/resetar/{email}/{forget}", "Web:reset", "web.reset");
+
 
 ob_end_flush();
